79d907543eab148d04c8a4a8da2dc682071e2542
itchyny <neknekneknek.t@gmail.com>
2013-02-24 01:37:00 +0900

    Revert "First pages commit"
    This reverts commit a3ae28817ebc35837c8e30b0bb19f5a4f1a78f75.
    
    Conflicts:
    
    	Canvas.js
    	README.md
    	index.html
    	main.css
    	main.js

--- Canvas.js
+++ Canvas.js
@@ -17,13 +17,9 @@
     }
   };
 
-<<<<<<< HEAD
   if (!global.Filter) return;
 
   if (global.Filter && global.Filter.Loader) return;
-=======
-  if (!global.Filter || !global.Loader) return;
->>>>>>> master
 
   if (global.Canvas) return;
 
@@ -267,70 +263,6 @@
            this.browserToCanvas(event)));
   };
 
-<<<<<<< HEAD
-  Filter.Loader = function FilterLoader(filter, image) {
-    this.filter = filter;
-    this.imageLarge = this.image = image;
-    this.canvas = undefined;
-  };
-
-  Filter.Loader.prototype.setLargeImage = function(image) {
-    this.imageLarge = image;
-  };
-
-  Filter.Loader.prototype.load = function(callback) {
-    if (this.loaded) {
-      callback(this.data);
-      return;
-    }
-    var cvs = document.createElement('canvas');
-    cvs.width = this.image.width;
-    cvs.height = this.image.height;
-    var ctx = cvs.getContext('2d');
-    var self = this;
-    this.convertToImage(this.filter.filter(this.image), function(data) {
-      self.loaded = true;
-      callback(self.data = data);
-      delete data.src;
-    });
-    return this.result;
-  };
-
-  Filter.Loader.prototype.loadLarge = function(callback) {
-    if (this.loadedLarge) {
-      callback.call(this, this.dataLarge);
-      return;
-    }
-    var cvs = document.createElement('canvas');
-    cvs.width = this.imageLarge.width;
-    cvs.height = this.imageLarge.height;
-    var ctx = cvs.getContext('2d');
-    var self = this;
-    this.filter.filterLazy(this.imageLarge, function(data) {
-      self.convertToImage(data, function(resultLarge) {
-        self.loadedLarge = true;
-        callback.call(self, self.dataLarge = resultLarge);
-      delete resultLarge.src;
-      });
-    });
-    return this.result;
-  };
-
-  Filter.Loader.prototype.convertToImage = function(data, callback) {
-    var cvs = document.createElement('canvas');
-    cvs.width = data.width;
-    cvs.height = data.height;
-    var ctx = cvs.getContext('2d');
-    ctx.putImageData(data, 0, 0);
-    var image = new Image();
-    image.onload = function(event) {
-      callback(image);
-    };
-    image.src = cvs.toDataURL();
-  };
-
-=======
->>>>>>> master
   global.Canvas = Canvas;
 
 })(this);
